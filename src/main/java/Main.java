330d15d6b6fe2b34934d8b624a1bb0a5900738f0
HP <mahletabateabate2@gmail.com>
2022-09-01 12:29:16 -0400

    home

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,23 +8,27 @@ public class Main {
         DrinkService drinkService = new DrinkService();
         boolean HaveCoffee = true;
         while (HaveCoffee){
-            System.out.println("WHAT CAN I GET YOU: (latte, white chocolate mocha, caramel macchiato,london fog tea latte ,exit");
+            System.out.println("what can i get you hot , iced , exit )");
             String UserInput = ab.nextLine();
-            if(UserInput.equals("latte")) {
+            if(UserInput.equals("hot")) {
+                System.out.println("latte ,white chocolate mocha , caramel macchiato");
+            }else if (UserInput.equals("latte")) {
                 System.out.println("espresso shot and milk");
-            } else if (UserInput.equals("white chocolate mocha")) {
+               } else if (UserInput.equals("white chocolate mocha")) {
                 System.out.println("espresso shot, white mocha, milk and top with wiped cream");
-            } else if (UserInput.equals("caramel macchiato")) {
+               } else if (UserInput.equals("caramel macchiato")) {
                 System.out.println("espresso shot, vanilla syrup, milk and topped with wiped cream and caramel sauce");
-            } else if (UserInput.equals("london fog tea latte")) {
-                System.out.println("english breakfast tea bag topped with steam milk");
-
-//            } else if(UserInput.equals("exit")) {
-//
-//                System.out.println("Thanks for Visiting");
-
-            } else if (UserInput.equals("exit")) {
+               } else if (UserInput.equals("london fog tea latte")) {
+                   System.out.println("english breakfast tea bag topped with steam milk");
+               }else if(UserInput.equals("iced")){
+                   System.out.println("vanilla been frappuccino,  green tea latte, ");
+               } else if (UserInput.equals("vanilla been frappuccino")) {
+                   System.out.println("milk ,vanilla been powder , blend with ice and top with wiped cream ");
+            } else if (UserInput.equals("green tea latte")) {
+                System.out.println("milk , scoop of matcha powder , ice, put in shaker and shake it well ");
+              } else if (UserInput.equals("exit")) {
               HaveCoffee =false;
+                System.out.println("THANKS FOR VISITING");
             }
         }
         }
